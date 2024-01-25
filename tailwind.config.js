d6a25547a98f839f7184d0f5b07a5538e78e245a
KH7NH <duckhanh050620003@gmail.com>
2024-01-25 23:38:09 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Karla: ['Karla', 'sans-serif']
+      },
+      colors:{
+        'light-coffee': "#C89F94",
+      }
+    },
+  },
+  plugins: [],
+}
+
